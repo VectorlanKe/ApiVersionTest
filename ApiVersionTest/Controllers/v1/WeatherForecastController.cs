b2bc1291a3b8ebaa29d3014a1207f3b48ab328bf
vectorluo <vectorluo@qq.com>
2023-02-07 16:09:19 +0800

    调整版本控制

--- ApiVersionTest/Controllers/v1/WeatherForecastController.cs
+++ ApiVersionTest/Controllers/v1/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace ApiVersionTest.Controllers
+namespace ApiVersionTest.Controllers.v1
 {
     /// <summary>
     /// 天气接口
@@ -28,23 +28,7 @@ namespace ApiVersionTest.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-        /// <summary>
-        /// 获取天气
-        /// </summary>
-        /// <returns></returns>
-        [ApiVersion("2.1")]
-        [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> GetWeather()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 2).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
--- ApiVersionTest/Program.cs
+++ ApiVersionTest/Program.cs
@@ -1,6 +1,6 @@
 using ApiVersionTest;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
@@ -15,6 +15,10 @@ builder.Services.AddApiVersioning(options =>
     options.ReportApiVersions = true;
     options.AssumeDefaultVersionWhenUnspecified = true;
     options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+//可配置header
+//options.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+//    new HeaderApiVersionReader("X-Accept-version"),
+//    new MediaTypeApiVersionReader("X-Accept-version"));
 });
 builder.Services.AddVersionedApiExplorer(options =>
 {
@@ -27,7 +31,8 @@ builder.Services.AddEndpointsApiExplorer();
 if (builder.Environment.IsDevelopment())
 {
     //添加swagger配置信息
-    builder.Services.AddSingleton<IConfigureOptions<SwaggerGenOptions>, SwaggerConfigure>();
+    builder.Services.AddSingleton<IConfigureOptions<SwaggerGenOptions>, SwaggerGenConfigure>();
+    builder.Services.AddSingleton<IConfigureOptions<SwaggerOptions>, SwaggerConfigure>();
     builder.Services.AddSingleton<IConfigureOptions<SwaggerUIOptions>, SwaggerUiConfigure>();
     builder.Services.AddSwaggerGen();
 }
@@ -38,14 +43,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(options =>
-    {
-        //options.PreSerializeFilters.Add((swagger, req) =>
-        //{
-        //    //添加目标服务器地址
-        //    swagger.Servers = new List<OpenApiServer>() { new OpenApiServer() { Url = $"{req.Scheme}://{req.Host}" } };
-        //});
-    });
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
